a6cf9eeef7c0a3d79a1848d960b068ff0d8f87b6
ARVIPE <arvipe@hotmail.com>
2020-11-12 13:28:01 +0100

    actualización

---
+++ includes/a_config.php
@@ -0,0 +1,15 @@
+<?php
+	switch ($_SERVER["SCRIPT_NAME"]) {
+		case "/about.php":
+			$CURRENT_PAGE = "About"; 
+			$PAGE_TITLE = "About Us";
+			break;
+		case "contact.php":
+			$CURRENT_PAGE = "Contact"; 
+			$PAGE_TITLE = "Contact Us";
+			break;
+		default:
+			$CURRENT_PAGE = "Index";
+			$PAGE_TITLE = "Bienvenido a ArCuPa";
+	}
+?>
