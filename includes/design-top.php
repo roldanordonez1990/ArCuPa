b0c09e4d56565c6905f453db78a568fcacbdf945
ARVIPE <arvipe@hotmail.com>
2020-11-13 09:52:16 +0100

    actualización

--- includes/design-top.php
+++
@@ -1,6 +0,0 @@
-<header class="header">
-    <div class="container">
-      <h1 class="site-title">ArCuPa</h1>
-    <span class="site-tagline">Servicio Metereológico de calidad</span>
-  </div>
-</header>
