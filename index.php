5d5d09dfe33a5054ff9a94b077488f9493e3a84c
adrianosuna27 <a.osu.alb.est@iesmarquesdecomares.org>
2020-11-16 11:23:29 +0100

    Distribucion

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<?php
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/MetaTags.php");?>
+</head>
+<body>
 
-echo "Hola mundo";
+<?php include("includes/header.php");?>
+<?php include("includes/navbar.php");?>
 
-?>
+
+<main>
+<section class="content">
+  <div class="full-bleed cool-photo">
+  
+  </div>
+</section>
+</main>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
