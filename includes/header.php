cc5d6fd15f407edcb10f03f06bd1fa30fe30bd4b
adrianosuna27 <a.osu.alb.est@iesmarquesdecomares.org>
2020-11-17 13:21:06 +0100

    seguimiento

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,17 @@
-<header class="header">
-    <div class="container">
-      <h1 class="site-title">Super Cool PHP Website!</h1>
-    <span class="site-tagline">Because flexbox and php is super cool!</span>
+<header class="container">
+<nav class="navbar bg-dark navbar-dark">
+  <!-- Logo -->
+  <a class="navbar-brand" href="#">
+  <img src="../media/images/logoFinal.png" alt="Logo">
+  </a>
+
+  <!--Búsqueda-->
+  <div class="ml-auto"> 
+  <form class="form-inline" action="">
+    <input class="form-control m-2" type="text" placeholder="Search">
+    <button class="btn btn-success fas fa-search" type="submit"></button>
+  </form>
   </div>
+    </div>
+</nav>
 </header>
