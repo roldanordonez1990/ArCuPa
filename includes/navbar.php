06d15dc6c9122e35338b202a785c4cec12bcc70a
ARVIPE <arvipe@hotmail.com>
2020-11-18 21:51:26 +0100

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,53 +1,60 @@
-<nav class="navbar navbar-expand-lg navbar-dark primary-color">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav w-100 justify-content-center">
-    <li class="nav item">
-      <button type="button" id="dropdownMenu1" data-toggle="dropdown"
-                    class="btn btn-success dropdown-toggle d-lg-none"><i class="fas fa-user pr-2"></i>Login</button>
-                <ul class="dropdown-menu dropdown-menu-right">
-                    <li class="px-3 py-2">
-                        <form class="form" role="form">
-                            <div class="form-group">
-                                <input id="emailInput" placeholder="Email" class="form-control form-control-sm"
-                                    type="text" required="">
-                            </div>
-                            <div class="form-group">
-                                <input id="passwordInput" placeholder="Contraseña" class="form-control form-control-sm"
-                                    type="text" required="">
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">Loguear</button>
-                            </div>
-                            <div class="form-group text-center">
-                                <small><a href="#" data-toggle="modal" data-target="#modalPassword">¿Olvidaste la
-                                        contraseña?</a></small>
-                            </div>
-                        </form>
-                    </li>
-                </ul>
-                <form class="form-inline d-lg-none" action="">
-    <input class="form-control m-2" type="text" placeholder="Search">
-    <button class="btn btn-success fas fa-search" type="submit"></button>
-  </form>
-      <li class="nav-item ">
-        <a class="nav-link text-center" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-center" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-center" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-center" href="#">Disabled</a>
-      </li>
 
+      <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav w-100 justify-content-center">
+        <li class="nav item d-lg-none">
+          <!-- Logo -->
+          <a class="nav-link" href="../index.php">
+                <img src="../media/images/logoFinal.png" alt="Logo">
+            </a>
+        </li>
+   
+          <li class="nav-item ">
+            <a class="nav-link text-center" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-center" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-center" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-center" href="#">Disabled</a>
+          </li>
+          <li class="nav item">
+          <button type="button" id="dropdownMenu1" data-toggle="dropdown"
+                        class="btn btn-success dropdown-toggle d-lg-none"><i class="fas fa-user pr-2"></i>Login</button>
+                    <ul class="dropdown-menu dropdown-menu-down">
+                        <li class="px-3 py-2">
+                            <form class="form" role="form">
+                                <div class="form-group">
+                                    <input id="emailInput" placeholder="Email" class="form-control form-control-sm"
+                                        type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input id="passwordInput" placeholder="Contraseña" class="form-control form-control-sm"
+                                        type="text" required="">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Loguear</button>
+                                </div>
+                                <div class="form-group text-center">
+                                    <small><a href="#" data-toggle="modal" data-target="#modalPassword">¿Olvidaste la
+                                            contraseña?</a></small>
+                                </div>
+                            </form>
+                        </li>
+                    </ul>
+                    <form class="form-inline d-lg-none" action="">
+        <input class="form-control m-2" type="text" placeholder="Search">
+        <button class="btn btn-success fas fa-search" type="submit"></button>
+      </form>
 
-    </ul>
-    
-  </div>
-</nav>
+        </ul>
+        
+      </div>
+      </nav>
