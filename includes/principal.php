14841e4268b41923e3e6032d406f812171310828
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-11-26 00:16:37 +0100

    Layout página principal fin

--- includes/principal.php
+++ includes/principal.php
@@ -2,16 +2,15 @@
     <div class="row">
 
         <div class="col-sm-8">
-            <div class="row mt-3">
+            <div class="row mt-4">
                 <div class="titulosPrincipal font-weight-bold col-sm-6 border rounded text-center border-primary mr-3">Mapa actual de la situación en España</div>
                 <div class="titulosPrincipal font-weight-bold col-sm-5 border rounded text-center border-primary">Búsqueda...</div>
-                <img class="img-fluid d-block pr-5 mt-2" src="../media/images/mapa.jpg"> 
+                <div><img class="contenedorImagen img-fluid d-block pr-5 mt-2" src="../media/images/mapa.jpg"></div>
             </div>
             
         </div>
-        
         <div class="col-sm-4">
-            <div class="row mt-3">
+            <div class="row mt-4">
                     <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary">Descripción</div>  
                     <div class="col-sm-12 border rounded text-center border-primary mt-2" >
                         <p id="descripciones">Pequeña introducción explicativa del contenido de los mapas mostrados a la izquierda y enlaces hacia la información meteorológica detallada de las principales ciudades andaluzas y españolas</p>
@@ -32,8 +31,97 @@
         </div>
     </div>
 
-    
-    
+    <div class="row">
+
+        <div class="col-sm-8">
+            <div class="row mt-4">
+                <div class="titulosPrincipal font-weight-bold col-sm-6 border rounded text-center border-primary mr-3">Mapa actual de la situación en España</div>
+                <div class="titulosPrincipal font-weight-bold col-sm-5 border rounded text-center border-primary">Búsqueda...</div>
+                <div><img class="img-fluid d-block pr-5 mt-2" src="../media/images/climograma.png"></div> 
+            </div>
+            
+        </div>
+        <div class="col-sm-4">
+            <div class="w3 row mt-4">
+                    <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary">Descripción</div>  
+                    <div class="contenedorImagen col-sm-12 border rounded text-center border-primary mt-2" >
+                        <p id="descripciones">Pequeña introducción explicativa del contenido de los mapas mostrados a la izquierda y enlaces hacia la información meteorológica detallada de las principales ciudades andaluzas y españolas</p>
+                        <ol>
+                            <a class="enlaceCiudades" href="#"><li>Córdoba</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Sevilla</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Málaga</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Granada</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Cádiz</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Huelva</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Almería</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Jaen</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Madrid</li></a>
+                            <a class="enlaceCiudades" href="#"><li>Barcelona</li></a>
+                        </ol>
+                    </div>
+            </div>
+        </div>
     </div>
-    
+
+    <div class="row">
+        <div class="col-sm-8">
+            <div><img class="img-fluid d-block pr-5 mt-3 mb-4" src="../media/images/textoImagen.jpg"></div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="row">
+                <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary mt-3">Multimedia..</div>  
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card-group">
+                       
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card-group">
+                        
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card-group">
+                      
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+            
+            </div>
+
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card-group">
+                       
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card-group">
+                        
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card-group">
+                      
+                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
+
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+
 </div>
