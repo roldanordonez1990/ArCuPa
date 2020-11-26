c6802fabfd3bdef566753acb632a7c64b9f41814
ARVIPE <arvipe@hotmail.com>
2020-11-26 11:53:53 +0100

    navbar actu

--- includes/MetaTags.php
+++ includes/MetaTags.php
@@ -17,10 +17,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
     type="text/css">
 <!--Tipografía-->
@@ -29,14 +26,8 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
-    type="text/css">
-
- 
 
 
 <link rel="stylesheet" href="css/mystyle.css">
--- includes/navbar.php
+++ includes/navbar.php
@@ -4,8 +4,8 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06"
         aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation"> <span
             class="navbar-toggler-icon"></span> </button>
-    <a class="nav-link text-center d-lg-none"> <img src="../media/images/logoFinal.png" alt="Logo"></a>
-    <button type="button" class="btn btn-success d-lg-none" data-toggle="modal" data-target="#logoutModal"><i
+    <a class="nav-link text-center d-lg-none"> <img class="imagen" src="../media/images/logoFinal.png" alt="Logo"></a>
+    <button type="button" class="btn btn-warning d-lg-none" data-toggle="modal" data-target="#logoutModal"><i
             class="fas fa-user pr-2"></i></button>
     <div class="collapse navbar-collapse" id="navbarsExample06">
         <ul class="navbar-nav navbar-center">
@@ -13,24 +13,24 @@
                 <div class="dropdown">
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><label>Mapas</label></a>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu backgroundcolor">
                     <li class="dropdown-submenu ">
                         <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false"><label>Observación</label></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" href="#"><label>Radar</label></a></li>
-                            <li><a tabindex="-1" href="#"><label>Satélite</label></a></li>
-                            <li><a tabindex="-1" href="#"><label>Rayos</label></a></li>
-                            <li><a tabindex="-1" href="#"><label>Masas</label></a></li>
+                        <ul class="dropdown-menu backgroundcolor">
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Radar</label></a></li>
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Satélite</label></a></li>
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Rayos</label></a></li>
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Masas</label></a></li>
                         </ul>
                     </li>
                     <li class="dropdown-submenu">
                         <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false"><label>Predicción</label></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" href="#"><label>Provincias</label></a></li>
-                            <li><a tabindex="-1" href="#"><label>Municipios</label></a></li>
-                            <li><a tabindex="-1" href="#"><label>CCAA</label></a></li>
+                        <ul class="dropdown-menu backgroundcolor">
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Provincias</label></a></li>
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">Municipios</label></a></li>
+                            <li><a tabindex="-1" href="#"><label class="colorLetra">CCAA</label></a></li>
                         </ul>
                     </li>
                 </ul>
@@ -39,7 +39,7 @@
             </li>
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><label>Mapas</label></a>
-                <div class="dropdown-menu" aria-labelledby="dropdown06">
+                <div class="dropdown-menu backgroundcolor" aria-labelledby="dropdown06">
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">Frentes</a>
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
@@ -50,7 +50,7 @@
             </li>
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><label>Multimedia</label></a>
-                <div class="dropdown-menu" aria-labelledby="dropdown06">
+                <div class="dropdown-menu backgroundcolor" aria-labelledby="dropdown06">
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">Nubes y cielos</a>
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
@@ -61,7 +61,7 @@
             </li>
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><label>Avisos</label></a>
-                <div class="dropdown-menu" aria-labelledby="dropdown06">
+                <div class="dropdown-menu backgroundcolor" aria-labelledby="dropdown06">
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">Nacionales</a>
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
@@ -72,7 +72,7 @@
             </li>
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="dropdown06" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><label>Noticias</label></a>
-                <div class="dropdown-menu" aria-labelledby="dropdown06">
+                <div class="dropdown-menu backgroundcolor" aria-labelledby="dropdown06">
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">Nacionales</a>
                     <a class="dropdown-item " id="dropdown07" data-toggle="dropdown" aria-haspopup="true"
