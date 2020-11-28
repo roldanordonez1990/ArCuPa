4d1cbaf13025a25b4c41739c7be4b38e811b3127
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-11-28 01:50:00 +0100

    página principal ¿terminada?

--- includes/principal.php
+++ includes/principal.php
@@ -3,17 +3,30 @@
 
         <div class="col-sm-8">
             <div class="row mt-4">
-                <div class="titulosPrincipal font-weight-bold col-sm-6 border rounded text-center border-primary mr-3">Mapa actual de la situación en España</div>
-                <div class="titulosPrincipal font-weight-bold col-sm-5 border rounded text-center border-primary">Búsqueda...</div>
-                <div><img class="contenedorImagen img-fluid d-block pr-5 mt-2" src="../media/images/mapa.jpg"></div>
+                <div class="titulosPrincipal font-weight-bold col-sm-12 text-left">Mapa actual de la situación meteorológica en España</div>
+                <div><img class="contenedorImagen img-fluid d-block pr-5 mt-1 ml-3" src="../media/images/mapa.jpg"></div>
             </div>
             
         </div>    
         <div class="col-sm-4">
-            <div class="row mt-4">
-                    <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary">Descripción</div>  
-                    <div class="col-sm-12 border rounded text-center border-primary mt-2" >
+        <div class="row mt-4">
+                    <div class="d-none d-md-block titulosPrincipal font-weight-bold col-sm-12 text-center">Descripción</div>  
+                    <div class="d-none d-md-block contenedorImagen col-sm-12 border visi rounded text-center border-primary mt-1" >
                         <p id="descripciones">Pequeña introducción explicativa del contenido de los mapas mostrados a la izquierda y enlaces hacia la información meteorológica detallada de las principales ciudades andaluzas y españolas</p>
+                        <div class="row mb-1">
+                            <i class="col-sm-4 fas fa-globe"></i>
+                            <i class="col-sm-4 fas fa-temperature-high"></i>
+                            <i class="col-sm-4 fas fa-cloud-rain"></i>
+                        </div>
+                        
+                        <div class="row mb-1">
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Ciudad</div>
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Temperatura</div>
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Probabilidad</div>
+                        </div>
+                        
+                    <div class="row">
+                        <div class="col-sm-4">
                         <ol>
                             <a class="enlaceCiudades" href="#"><li>Córdoba</li></a>
                             <a class="enlaceCiudades" href="#"><li>Sevilla</li></a>
@@ -26,7 +39,37 @@
                             <a class="enlaceCiudades" href="#"><li>Madrid</li></a>
                             <a class="enlaceCiudades" href="#"><li>Barcelona</li></a>
                         </ol>
+                        </div>
+                        <div class="col-sm-4">
+                        <ul id="noNeeded">
+                            <a class="enlaceCiudades"><li>45ºC</li></a>
+                            <a class="enlaceCiudades"><li>43ºC</li></a>
+                            <a class="enlaceCiudades"><li>24ºC</li></a>
+                            <a class="enlaceCiudades"><li>12ºC</li></a>
+                            <a class="enlaceCiudades"><li>25ºC</li></a>
+                            <a class="enlaceCiudades"><li>23ºC</li></a>
+                            <a class="enlaceCiudades"><li>33ºC</li></a>
+                            <a class="enlaceCiudades"><li>41ºC</li></a>
+                            <a class="enlaceCiudades"><li>36ºC</li></a>
+                            <a class="enlaceCiudades"><li>27ºC</li></a>
+                        </ul>
+                        </div>
+                        <div class="col-sm-4">
+                        <ul id="noNeeded">
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>10%</li></a>
+                            <a class="enlaceCiudades"><li>5%</li></a>
+                            <a class="enlaceCiudades"><li>2%</li></a>
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>100%</li></a>
+                            <a class="enlaceCiudades"><li>40%</li></a>
+                            <a class="enlaceCiudades"><li>99%</li></a>
+                            <a class="enlaceCiudades"><li>5%</li></a>
+                        </ul>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
@@ -35,17 +78,29 @@
 
         <div class="col-sm-8">
             <div class="row mt-4">
-                <div class="titulosPrincipal font-weight-bold col-sm-6 border rounded text-center border-primary mr-3">Mapa actual de la situación en España</div>
-                <div class="titulosPrincipal font-weight-bold col-sm-5 border rounded text-center border-primary">Búsqueda...</div>
-                <div><img class="img-fluid d-block pr-5 mt-2" src="../media/images/climograma.png"></div> 
+                <div class="titulosPrincipal font-weight-bold col-sm-6 text-left">Climograma actualizado de tu municipio</div>
+                <div><img class="img-fluid d-block pr-5 mt-1" src="../media/images/climograma.png"></div> 
             </div>
             
         </div>
         <div class="col-sm-4">
-            <div class="w3 row mt-4">
-                    <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary">Descripción</div>  
-                    <div class="contenedorImagen col-sm-12 border rounded text-center border-primary mt-2" >
+            <div class="row mt-4">
+                    <div class="d-none d-md-block titulosPrincipal font-weight-bold col-sm-12 text-center">Descripción</div>  
+                    <div class="d-none d-md-block contenedorImagen col-sm-12 visi border rounded text-center border-primary mt-1" >
                         <p id="descripciones">Pequeña introducción explicativa del contenido de los mapas mostrados a la izquierda y enlaces hacia la información meteorológica detallada de las principales ciudades andaluzas y españolas</p>
+                        
+                        <div class="row mb-1">
+                            <i class="col-sm-4 fas fa-globe"></i>
+                            <i class="col-sm-4 fas fa-temperature-high"></i>
+                            <i class="col-sm-4 fas fa-cloud-rain"></i>
+                        </div>
+                        <div class="row mb-1">
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Ciudad</div>
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Temperatura</div>
+                            <div class="letraPrincipal col-sm-4 font-weight-bold text-center">Probabilidad</div>
+                        </div>
+                    <div class="row">
+                        <div class="col-sm-4">
                         <ol>
                             <a class="enlaceCiudades" href="#"><li>Córdoba</li></a>
                             <a class="enlaceCiudades" href="#"><li>Sevilla</li></a>
@@ -58,70 +113,64 @@
                             <a class="enlaceCiudades" href="#"><li>Madrid</li></a>
                             <a class="enlaceCiudades" href="#"><li>Barcelona</li></a>
                         </ol>
+                        </div>
+                        <div class="col-sm-4">
+                        <ul id="noNeeded">
+                            <a class="enlaceCiudades"><li>45ºC</li></a>
+                            <a class="enlaceCiudades"><li>43ºC</li></a>
+                            <a class="enlaceCiudades"><li>24ºC</li></a>
+                            <a class="enlaceCiudades"><li>12ºC</li></a>
+                            <a class="enlaceCiudades"><li>25ºC</li></a>
+                            <a class="enlaceCiudades"><li>23ºC</li></a>
+                            <a class="enlaceCiudades"><li>33ºC</li></a>
+                            <a class="enlaceCiudades"><li>41ºC</li></a>
+                            <a class="enlaceCiudades"><li>36ºC</li></a>
+                            <a class="enlaceCiudades"><li>27ºC</li></a>
+                        </ul>
+                        </div>
+                        <div class="col-sm-4">
+                        <ul id="noNeeded">
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>10%</li></a>
+                            <a class="enlaceCiudades"><li>5%</li></a>
+                            <a class="enlaceCiudades"><li>2%</li></a>
+                            <a class="enlaceCiudades"><li>0%</li></a>
+                            <a class="enlaceCiudades"><li>100%</li></a>
+                            <a class="enlaceCiudades"><li>40%</li></a>
+                            <a class="enlaceCiudades"><li>99%</li></a>
+                            <a class="enlaceCiudades"><li>5%</li></a>
+                        </ul>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="row">
+    <div class="row mb-4">
         <div class="col-sm-8">
-            <div><img class="img-fluid d-block pr-5 mt-3 mb-4" src="../media/images/textoImagen.jpg"></div>
+            <p class="mt-3" id="descripciones">Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent volutpat vulputate massa quis himenaeos interdum, risus ac tristique semper cum enim. Lobortis vitae cum nisl integer volutpat consequat litora fermentum mus dictum, odio taciti netus cras diam pellentesque eu mauris gravida dictumst, mi pretium imperdiet metus molestie purus sodales vehicula hac. Primis curabitur torquent risus eros ligula ad euismod neque himenaeos ante facilisis, erat dictum nullam maecenas tellus mollis tempus rhoncus cursus.Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent volutpat vulputate massa quis himenaeos interdum, risus ac tristique semper cum enim. Lobortis vitae cum nisl integer volutpat consequat litora fermentum mus dictum, odio taciti netus cras diam pellentesque eu mauris gravida dictumst, mi pretium imperdiet metus molestie purus sodales vehicula hac. Primis curabitur torquent risus eros ligula ad euismod neque himenaeos ante facilisis, erat dictum nullam maecenas tellus mollis tempus rhoncus cursus</p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <iframe width="695" height="300" src="https://www.youtube.com/embed/uXJFnZjk-4w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
-
+        
         <div class="col-sm-4">
             <div class="row">
-                <div class="titulosPrincipal font-weight-bold col-sm-12 border rounded text-center border-primary mt-3">Multimedia..</div>  
+                <div class="titulosPrincipal font-weight-bold col-sm-12 text-center mt-3">Multimedia</div>  
             </div>
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-md-12">
                     <div class="card-group">
-                       
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
+                        <div class=""><img class="multi multiImagen mt-2" src="../media/images/nube.jpg"></div>
+                        <div class=""><img class="multi multiImagen mt-2" src="../media/images/paisajes.jpg"></div>
+                        <div class=""><img class="multi multiImagen mt-2" src="../media/images/videos.jpg"></div>
                     </div>
                 </div>
-                <div class="col-sm-4">
-                    <div class="card-group">
-                        
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="card-group">
-                      
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
-                    </div>
-                </div>
-            
-            </div>
-
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="card-group">
-                       
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="card-group">
-                        
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="card-group">
-                      
-                        <img class="img-fluid d-block mt-3 mb-4" src="../media/images/paisaje1.jpg">
-
-                    </div>
-                </div>
-            
             </div>
         </div>
     </div>
-
 </div>
