efbb508b6ff33ca001ee6284730993a8a9ba975e
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-12-02 19:40:47 +0100

    fin páginas

--- includes/footer.php
+++ includes/footer.php
@@ -1,55 +1,56 @@
 <!-- Footer -->
 <footer class="fixed">
 
-  <!-- Footer Elements -->
-  <div class="container">
+    <!-- Footer Elements -->
+    <div class="container">
 
-    <!-- Grid row-->
-    <div class="row">
+        <!-- Grid row-->
+        <div class="row">
 
-      <!-- Grid column -->
-      <div class="col-sm-12 py-4"> <!--con la clase py obtenemos el alto del div-->
-      
-        <div class="iconos mb-2">
+            <!-- Grid column -->
+            <div class="col-sm-12 py-4">
+                <!--con la clase py obtenemos el alto del div-->
 
-          <!-- Facebook -->
-          <a href="" class="rrss icono">
-            <i class="fab fa-facebook-f fa-lg mr-4 mt-1 fa-2x"> </i>
-          </a>
-          <!-- Twitter -->
-          <a href="" class="rrss icono">
-            <i class="fab fa-twitter fa-lg mr-4 mt-1 fa-2x"> </i>
-          </a>
-          <!--Instagram-->
-          <a href="" class="rrss icono">
-            <i class="fab fa-instagram fa-lg mr-4 mt-1 fa-2x"> </i>
-          </a>
-           <!--YouTube-->
-           <a href="" class="rrss icono">
-           <i class="fab fa-youtube fa-lg mr-4 mt-1 fa-2x"></i>
-          </a>
-          <!--Google-->
-          <a href="" class="rrss icono">
-          <i class="fab fa-google-plus-g fa-lg mr-4 mt-1 fa-2x"></i>
-          </a>
-          
+                <div class="iconos mb-2 ml-4">
+
+                    <!-- Facebook -->
+                    <a href="" class="rrss icono">
+                        <i class="fab fa-facebook-f fa-lg mr-4 mt-1 fa-2x"> </i>
+                    </a>
+                    <!-- Twitter -->
+                    <a href="" class="rrss icono">
+                        <i class="fab fa-twitter fa-lg mr-4 mt-1 fa-2x"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a href="" class="rrss icono">
+                        <i class="fab fa-instagram fa-lg mr-4 mt-1 fa-2x"> </i>
+                    </a>
+                    <!--YouTube-->
+                    <a href="" class="rrss icono">
+                        <i class="fab fa-youtube fa-lg mr-4 mt-1 fa-2x"></i>
+                    </a>
+                    <!--Google-->
+                    <a href="" class="rrss icono">
+                        <i class="fab fa-google-plus-g fa-lg mr-4 mt-1 fa-2x"></i>
+                    </a>
+
+                </div>
+            </div>
+            <!-- Grid column -->
+
         </div>
-      </div>
-      <!-- Grid column -->
+        <!-- Grid row-->
 
     </div>
-    <!-- Grid row-->
+    <!-- Footer Elements -->
 
-  </div>
-  <!-- Footer Elements -->
-
-  <!-- Copyright -->
-  <div class="footer icono text-center">
-     www.arcupa.es - Servicio Web Meteorológico Oficial
-  </div>
-  <div class="footer text-center py-3">© 2020 Copyright:
-    <a class="footer icono" href="">Términos Legales/Legal Termns</a>
-  </div>
-  <!-- Copyright -->
+    <!-- Copyright -->
+    <div class="footer icono text-center">
+        www.arcupa.es - Servicio Web Meteorológico Oficial
+    </div>
+    <div class="footer text-center py-3">© 2020 Copyright:
+        <a class="footer icono" href="">Términos Legales/Legal Termns</a>
+    </div>
+    <!-- Copyright -->
 </footer>
 <!-- Footer -->
