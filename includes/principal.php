bcd35bae072d0463007d0597c4f89b804d1b056c
Francisco Roldan <roldanordonez.francisco@gmail.com>
2020-12-02 10:00:29 +0100

    final

--- includes/principal.php
+++ includes/principal.php
@@ -110,7 +110,7 @@
         <div class="col-sm-4">
             <div class="row mt-4">
                     <div class="d-none d-lg-block titulosPrincipal font-weight-bold col-sm-12 text-center">Descripción</div>  
-                    <div class="d-none d-lg-block contenedorImagen col-sm-12 visi border rounded text-center border-primary mt-1" >
+                    <div class="d-none d-lg-block contenedorImagen col-sm-12 visi border rounded text-center border-primary" >
                         <p id="descripciones">Pequeña introducción explicativa del contenido de los mapas mostrados a la izquierda y enlaces hacia la información meteorológica detallada de las principales ciudades andaluzas y españolas</p>
                         <table class="medidasTabla ">
                                 <tr>
@@ -202,7 +202,7 @@
             <p class="mt-3" id="descripciones">Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent volutpat vulputate massa quis himenaeos interdum, risus ac tristique semper cum enim. Lobortis vitae cum nisl integer volutpat consequat litora fermentum mus dictum, odio taciti netus cras diam pellentesque eu mauris gravida dictumst, mi pretium imperdiet metus molestie purus sodales vehicula hac. Primis curabitur torquent risus eros ligula ad euismod neque himenaeos ante facilisis, erat dictum nullam maecenas tellus mollis tempus rhoncus cursus.Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent volutpat vulputate massa quis himenaeos interdum, risus ac tristique semper cum enim. Lobortis vitae cum nisl integer volutpat consequat litora fermentum mus dictum, odio taciti netus cras diam pellentesque eu mauris gravida dictumst, mi pretium imperdiet metus molestie purus sodales vehicula hac. Primis curabitur torquent risus eros ligula ad euismod neque himenaeos ante facilisis, erat dictum nullam maecenas tellus mollis tempus rhoncus cursus</p>
         </div>
         
-        <div class="col-lg-4 ">
+        <div class="col-lg-4">
             <div class="row mb-2">
                 <div class="titulosPrincipal font-weight-bold col-sm-12 text-center mt-3">Multimedia</div>  
             </div>
