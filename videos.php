917f81853a74dae017b25851ad6c8cc48992065b
ARVIPE <arvipe@hotmail.com>
2020-12-02 10:10:29 +0100

    videos

--- videos.php
+++ videos.php
@@ -17,46 +17,45 @@
         <div class="titulosPrincipal text-center p-3">
                     <h1>Videos</h1>
             </div>
-        
-                <div id="video-carousel-example" class="carousel slide carousel-fade pb-3">
-                    <!--Indicators-->
-                    <ol class="carousel-indicators">
-                        <li data-target="#video-carousel-example" data-slide-to="0" class="active"></li>
-                        <li data-target="#video-carousel-example" data-slide-to="1"></li>
-                        <li data-target="#video-carousel-example" data-slide-to="2"></li>
-                    </ol>
-                    <!--/.Indicators-->
-                    <!--Slides-->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <video class="video-fluid" autoplay loop muted>
-                                <source src="https://mdbootstrap.com/img/video/Tropical.mp4" type="video/mp4" />
-                            </video>
-                        </div>
-                        <div class="carousel-item">
-                            <video class="video-fluid" autoplay loop muted>
-                                <source src="https://mdbootstrap.com/img/video/forest.mp4" type="video/mp4" />
-                            </video>
-                        </div>
-                        <div class="carousel-item">
-                            <video class="video-fluid" autoplay loop muted>
-                                <source src="https://mdbootstrap.com/img/video/Agua-natural.mp4" type="video/mp4" />
-                            </video>
-                        </div>
-                    </div>
-                    <!--/.Slides-->
-                    <!--Controls-->
-                    <a class="carousel-control-prev" href="#video-carousel-example" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#video-carousel-example" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    <!--/.Controls-->
-                </div>
-                <!--Carousel Wrapper-->
+        <!-- Carousel row -->
+        <div class="row">
+	                <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+	                	<!-- Carousel -->
+	                	<div id="carousel-example" class="carousel slide">
+	       					<ol class="carousel-indicators">
+	       						<li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+	       						<li data-target="#carousel-example" data-slide-to="1"></li>
+	       						<li data-target="#carousel-example" data-slide-to="2"></li>
+	       					</ol>
+	       					<div class="carousel-inner">
+	       						<div class="carousel-item active">
+	       							<div class="embed-responsive embed-responsive-16by9">
+	       								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6hgVihWjK2c?rel=0" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+	       						<div class="carousel-item">
+	       							<div class="embed-responsive embed-responsive-16by9">
+	       								<iframe class="embed-responsive-item" src="https://player.vimeo.com/video/84910153?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+	       						<div class="carousel-item">
+	       							<div class="embed-responsive embed-responsive-16by9">
+	       								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/oiKj0Z_Xnjc" allowfullscreen></iframe>
+	       							</div>
+	       						</div>
+	       					</div>
+							<a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+	       				</div>
+	                	<!-- End carousel -->
+	                </div>
+	            </div>
             
         </main>
 
