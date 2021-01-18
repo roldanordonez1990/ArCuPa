8d20cd55528863bc00008c0651a50d9a77bc3452
adrianosuna27 <a.osu.alb.est@iesmarquesdecomares.org>
2021-01-18 10:43:13 +0100

    cambios

--- noticiasProvinciales.php
+++ noticiasProvinciales.php
@@ -24,61 +24,108 @@
 
                                     <!-- Single Blog Start -->
                                     <div class="col-12">
-                                        <div class="single-blog-post mb-50 wow fadeInUp" >
+                                        <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
                                                 <img class="img" src="../media/images/tormenta.jpg " alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <a href="#" class="post-title">Tormenta subtropical Alpha (2020)</a>
+                                            <h1 class="titulosPrincipal">Los últimos coletazos de Filomena tiñen de
+                                                blanco la sierra de Cabra</h1>
                                             <!-- Post Meta -->
-                                            <div  class="letra1">
-                                                <p>By <a href="#">Arturo Vicente</a> | <a href="#">Marzo 18, 2020</a></p>
+                                            <div class="letra1">
+                                                <p>Por Adrian Osuna | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que tocó tierra en Portugal, la primera ocurrencia registrada en el país. El sistema también fue el ciclón tropical atlántico de formación más oriental registrado en la cuenca, superando el récord anterior de la tormenta tropical Christine en 1973.</p>
+                                            <p>La zona de la Sierra de Cabra, a unos 1.200 metros de altitud, ha vuelto
+                                                a recibir la visita de la nieve por tercera vez en pocas semanas. Los
+                                                alrededores del Santuario de la Virgen de la Sierra han mantenido
+                                                durante la jornada una delicada capa blanca, que se ha mantenido estable
+                                                gracias a las bajas temperaturas, de entre uno y dos grados centígrados,
+                                                e incluso de la caída de nieve granulada en algunos momentos de la
+                                                mañana.</p>
+
+                                            <p>A medida que han ido pasando las horas las zonas como el Picacho de
+                                                Cabra se han ido llenando de gente, desde los fotógrafos dispuestos a
+                                                recoger las mejores instantáneas de unos campos bañados de blanco por la
+                                                nevada hasta familias completas, dispuestas a disfrutar de tan poco
+                                                habitual paisaje, tirar unas bolas de nieve, construir un muñeco o
+                                                lanzarse pendiente abajo con un tríneo, como puede verse en nuestra
+                                                galería de fotos.</p>
                                             <!-- Read More btn -->
-                                            <a href="#" class="btn academy-btn btn-sm mt-15">Leer Mas</a>
+                                            <!--<a href="" class="btn btn-login mt-15">Leer Mas</a>-->
                                         </div>
                                     </div>
 
                                     <!-- Single Blog Start -->
                                     <div class="col-12">
-                                        <div class="single-blog-post mb-50 wow fadeInUp" >
+                                        <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
                                                 <img class="img" src="../media/images/tormenta.jpg " alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <a href="#" class="post-title">Tormenta subtropical Alpha (2020)</a>
+                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
                                             <!-- Post Meta -->
-                                            <div  class="letra1">
-                                                <p>By <a href="#">Arturo Vicente</a> | <a href="#">Marzo 18, 2020</a></p>
+                                            <div class="letra1">
+                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que tocó tierra en Portugal, la primera ocurrencia registrada en el país. El sistema también fue el ciclón tropical atlántico de formación más oriental registrado en la cuenca, superando el récord anterior de la tormenta tropical Christine en 1973.</p>
+                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
+                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
+                                                sistema también fue el ciclón tropical atlántico de formación más
+                                                oriental registrado en la cuenca, superando el récord anterior de la
+                                                tormenta tropical Christine en 1973.</p>
                                             <!-- Read More btn -->
-                                            <a href="#" class="btn academy-btn btn-sm mt-15">Leer Mas</a>
+                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
                                         </div>
                                     </div>
 
                                     <!-- Single Blog Start -->
                                     <div class="col-12">
-                                        <div class="single-blog-post mb-50 wow fadeInUp" >
+                                        <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
                                                 <img class="img" src="../media/images/tormenta.jpg " alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <a href="#" class="post-title">Tormenta subtropical Alpha (2020)</a>
+                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
                                             <!-- Post Meta -->
-                                            <div  class="letra1">
-                                                <p>By <a href="#">Arturo Vicente</a> | <a href="#">Marzo 18, 2020</a></p>
+                                            <div class="letra1">
+                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que tocó tierra en Portugal, la primera ocurrencia registrada en el país. El sistema también fue el ciclón tropical atlántico de formación más oriental registrado en la cuenca, superando el récord anterior de la tormenta tropical Christine en 1973.</p>
+                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
+                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
+                                                sistema también fue el ciclón tropical atlántico de formación más
+                                                oriental registrado en la cuenca, superando el récord anterior de la
+                                                tormenta tropical Christine en 1973.</p>
                                             <!-- Read More btn -->
-                                            <a href="#" class="btn academy-btn btn-sm mt-15">Leer Mas</a>
+                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
+                                        </div>
+                                    </div>
+
+                                    <!-- Single Blog Start -->
+                                    <div class="col-12">
+                                        <div class="single-blog-post mb-50 wow fadeInUp">
+                                            <!-- Post Thumb -->
+                                            <div class="blog-post-thumb mb-50">
+                                                <img class="img" src="../media/images/tormenta.jpg " alt="">
+                                            </div>
+                                            <!-- Post Title -->
+                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
+                                            <!-- Post Meta -->
+                                            <div class="letra1">
+                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
+                                            </div>
+                                            <!-- Post Excerpt -->
+                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
+                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
+                                                sistema también fue el ciclón tropical atlántico de formación más
+                                                oriental registrado en la cuenca, superando el récord anterior de la
+                                                tormenta tropical Christine en 1973.</p>
+                                            <!-- Read More btn -->
+                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
                                         </div>
                                     </div>
 
@@ -92,51 +139,51 @@
                                 <!-- Latest Blog Posts Area -->
                                 <div class="latest-blog-posts mb-30">
                                     <h5 class="letra">Últimas noticias</h6>
-                                    <!-- Single Latest Blog Post -->
-                                    <div class="single-latest-blog-post d-flex mb-30">
-                                        <div class="latest-blog-post-thumb">
-                                            <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                        <!-- Single Latest Blog Post -->
+                                        <div class="single-latest-blog-post d-flex mb-30">
+                                            <div class="latest-blog-post-thumb">
+                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                            </div>
+                                            <div class="latest-blog-post-content">
+                                                <a href="#" class="post-title">
+                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                </a>
+                                                <a href="#" class="letra1">March 18, 2018</a>
+                                            </div>
                                         </div>
-                                        <div class="latest-blog-post-content">
-                                            <a href="#" class="post-title">
-                                                <h6 class="">Tormenta Subtropical Alpha</h6>
-                                            </a>
-                                            <a href="#" class="letra1">March 18, 2018</a>
+                                        <div class="single-latest-blog-post d-flex mb-30">
+                                            <div class="latest-blog-post-thumb">
+                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                            </div>
+                                            <div class="latest-blog-post-content">
+                                                <a href="#" class="post-title">
+                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                </a>
+                                                <a href="#" class="letra1">Marzo 18, 2018</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="single-latest-blog-post d-flex mb-30">
-                                        <div class="latest-blog-post-thumb">
-                                            <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                        <div class="single-latest-blog-post d-flex mb-30">
+                                            <div class="latest-blog-post-thumb">
+                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                            </div>
+                                            <div class="latest-blog-post-content">
+                                                <a href="#" class="post-title">
+                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                </a>
+                                                <a href="#" class="letra1">Marzo 18, 2018</a>
+                                            </div>
                                         </div>
-                                        <div class="latest-blog-post-content">
-                                            <a href="#" class="post-title">
-                                                <h6 class="">Tormenta Subtropical Alpha</h6>
-                                            </a>
-                                            <a href="#" class="letra1">Marzo 18, 2018</a>
+                                        <div class="single-latest-blog-post d-flex mb-30">
+                                            <div class="latest-blog-post-thumb">
+                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                            </div>
+                                            <div class="latest-blog-post-content">
+                                                <a href="#" class="post-title">
+                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                </a>
+                                                <a href="#" class="letra1">Marzo 18, 2018</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="single-latest-blog-post d-flex mb-30">
-                                        <div class="latest-blog-post-thumb">
-                                            <img class="img" src="../media/images/tormenta.jpg" alt="">
-                                        </div>
-                                        <div class="latest-blog-post-content">
-                                            <a href="#" class="post-title">
-                                                <h6 class="">Tormenta Subtropical Alpha</h6>
-                                            </a>
-                                            <a href="#" class="letra1">Marzo 18, 2018</a>
-                                        </div>
-                                    </div>
-                                    <div class="single-latest-blog-post d-flex mb-30">
-                                        <div class="latest-blog-post-thumb">
-                                            <img class="img" src="../media/images/tormenta.jpg" alt="">
-                                        </div>
-                                        <div class="latest-blog-post-content">
-                                            <a href="#" class="post-title">
-                                                <h6 class="">Tormenta Subtropical Alpha</h6>
-                                            </a>
-                                            <a href="#" class="letra1">Marzo 18, 2018</a>
-                                        </div>
-                                    </div>
                                 </div>
 
                             </div>
