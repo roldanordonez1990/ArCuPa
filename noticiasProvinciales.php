ea5b8b19808da64bc3f9d8e81b36f9382f6586f3
adrianosuna27 <a.osu.alb.est@iesmarquesdecomares.org>
2021-01-18 12:10:01 +0100

    mascontenido

--- noticiasProvinciales.php
+++ noticiasProvinciales.php
@@ -27,7 +27,7 @@
                                         <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
-                                                <img class="img" src="../media/images/tormenta.jpg " alt="">
+                                                <img class="img" src="../media/images/noticias/NieveCabra.jpg" alt="">
                                             </div>
                                             <!-- Post Title -->
                                             <h1 class="titulosPrincipal">Los últimos coletazos de Filomena tiñen de
@@ -62,22 +62,27 @@
                                         <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
-                                                <img class="img" src="../media/images/tormenta.jpg " alt="">
+                                                <img class="img" src="../media/images/noticias/LluviaSemanaSanta.jpg"
+                                                    alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
+                                            <h1 class="titulosPrincipal">Posibles lluvias en Semana Santa</h1>
                                             <!-- Post Meta -->
                                             <div class="letra1">
-                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
+                                                <p>Por Arturo Vicente | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
-                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
-                                                sistema también fue el ciclón tropical atlántico de formación más
-                                                oriental registrado en la cuenca, superando el récord anterior de la
-                                                tormenta tropical Christine en 1973.</p>
+                                            <p>Aunque aún es pronto para contar con previsiones certeras sobre el tiempo
+                                                que nos espera de cara a los próximos días, los datos de los que se
+                                                dispone hasta el momento apuntan a una Semana Santa con tiempo revuelto
+                                                e intermitentemente pasada por agua.</p>
+
+                                            <p>Aunque en los próximos días, a medida que se acerque la fecha, se
+                                                afinarán las predicciones, la Agencia Estatal de Meteorología (AEMET)
+                                                anuncia una alta probabilidad de lluvia durante el primer fin de semana,
+                                                del 18 al 20 de marzo "aunque serían débiles en general".</p>
                                             <!-- Read More btn -->
-                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
+                                            <!--<a href="" class="btn btn-login mt-15">Leer Mas</a>-->
                                         </div>
                                     </div>
 
@@ -86,22 +91,27 @@
                                         <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
-                                                <img class="img" src="../media/images/tormenta.jpg " alt="">
+                                                <img class="img" src="../media/images/noticias/EncinaRute.jpg" alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
+                                            <h1 class="titulosPrincipal">Quinto lugar para la encina milenaria de Rute
+                                                en el concurso de árbol del año de España</h1>
                                             <!-- Post Meta -->
                                             <div class="letra1">
-                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
+                                                <p>Por Francisco Roldán | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
-                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
-                                                sistema también fue el ciclón tropical atlántico de formación más
-                                                oriental registrado en la cuenca, superando el récord anterior de la
-                                                tormenta tropical Christine en 1973.</p>
+                                            <p>Finalmente ha sido otra encina, la carrasca milenaria de Lecina, la
+                                                elegida árbol del año de España y pasará a la selección del Árbol
+                                                Europeo de 2021. Selección en la que participaba la encina milenaria de
+                                                Rute que se ha quedado en quinto lugar, entro los doce árboles
+                                                monumentales seleccionados para esta edición. Los resultados de la
+                                                votación de Árbol del Año en España han colocado como ganadora, con
+                                                9.920 votos, a la carrasca de Lecina, de la Sierra de Guara de Huesca.
+                                                Mientras que la encina milenaria de Rute, en la comarca de la Subbética
+                                                cordobesa ha conseguido 2.242 votos.</p>
                                             <!-- Read More btn -->
-                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
+                                            <!--<a href="" class="btn btn-login mt-15">Leer Mas</a>-->
                                         </div>
                                     </div>
 
@@ -110,22 +120,28 @@
                                         <div class="single-blog-post mb-50 wow fadeInUp">
                                             <!-- Post Thumb -->
                                             <div class="blog-post-thumb mb-50">
-                                                <img class="img" src="../media/images/tormenta.jpg " alt="">
+                                                <img class="img" src="../media/images/noticias/NieveOlivos.jpg"
+                                                    alt="">
                                             </div>
                                             <!-- Post Title -->
-                                            <h1 class="titulosPrincipal">Tormenta subtropical Alpha (2020)</h1>
+                                            <h1 class="titulosPrincipal">Las heladas de Filomena destrozan parte de la
+                                                cosecha.</h1>
                                             <!-- Post Meta -->
                                             <div class="letra1">
-                                                <p>By Arturo Vicente | Marzo 18, 2020</p>
+                                                <p>Por Adrián Osuna | Marzo 18, 2020</p>
                                             </div>
                                             <!-- Post Excerpt -->
-                                            <p>La tormenta subtropical Alpha fue un ciclón subtropical muy inusual que
-                                                tocó tierra en Portugal, la primera ocurrencia registrada en el país. El
-                                                sistema también fue el ciclón tropical atlántico de formación más
-                                                oriental registrado en la cuenca, superando el récord anterior de la
-                                                tormenta tropical Christine en 1973.</p>
+                                            <p>El paso de Filomena no ha sido del todo agradable para el sector agrario.
+                                                Depende de las zonas donde ha actuado la borrasca ha causado graves
+                                                daños en la cosecha de la aceiutuna que algunos agricultores han llegado
+                                                a perder el 40% de la cosecha por las nevadas y las heladas en sus
+                                                terrenos.</p>
+                                            <p>Todo este daño causado por la borrasca puede influir en el precio del
+                                                aceite del proximo año. Los agricultores trabajan sin para para intentar
+                                                salvar el año, pero es verdad que el paso de Filomena ha complicado un
+                                                poco dicha tarea.</p>
                                             <!-- Read More btn -->
-                                            <a href="" class="btn btn-login mt-15">Leer Mas</a>
+                                            <!--<a href="" class="btn btn-login mt-15">Leer Mas</a>-->
                                         </div>
                                     </div>
 
@@ -142,44 +158,44 @@
                                         <!-- Single Latest Blog Post -->
                                         <div class="single-latest-blog-post d-flex mb-30">
                                             <div class="latest-blog-post-thumb">
-                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                                <img class="img" src="../media/images/noticias/NieveCabra.jpg" alt="">
                                             </div>
                                             <div class="latest-blog-post-content">
                                                 <a href="#" class="post-title">
-                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                    <h6 class="">Filomena por Cabra</h6>
                                                 </a>
                                                 <a href="#" class="letra1">March 18, 2018</a>
                                             </div>
                                         </div>
                                         <div class="single-latest-blog-post d-flex mb-30">
                                             <div class="latest-blog-post-thumb">
-                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                                <img class="img" src="../media/images/noticias/LluviaSemanaSanta.jpg" alt="">
                                             </div>
                                             <div class="latest-blog-post-content">
                                                 <a href="#" class="post-title">
-                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                    <h6 class="">Lluvias en Semana Santa</h6>
                                                 </a>
                                                 <a href="#" class="letra1">Marzo 18, 2018</a>
                                             </div>
                                         </div>
                                         <div class="single-latest-blog-post d-flex mb-30">
                                             <div class="latest-blog-post-thumb">
-                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                                <img class="img" src="../media/images/noticias/EncinaRute.jpg" alt="">
                                             </div>
                                             <div class="latest-blog-post-content">
                                                 <a href="#" class="post-title">
-                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                    <h6 class="">Quinto lugar para Rute</h6>
                                                 </a>
                                                 <a href="#" class="letra1">Marzo 18, 2018</a>
                                             </div>
                                         </div>
                                         <div class="single-latest-blog-post d-flex mb-30">
                                             <div class="latest-blog-post-thumb">
-                                                <img class="img" src="../media/images/tormenta.jpg" alt="">
+                                                <img class="img" src="../media/images/noticias/NieveOlivos.jpg" alt="">
                                             </div>
                                             <div class="latest-blog-post-content">
                                                 <a href="#" class="post-title">
-                                                    <h6 class="">Tormenta Subtropical Alpha</h6>
+                                                    <h6 class="">Perdida de Cosechas</h6>
                                                 </a>
                                                 <a href="#" class="letra1">Marzo 18, 2018</a>
                                             </div>
