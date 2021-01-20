5dee197e538b29918711ff5eea545fcebb787d77
Francisco Roldan <roldanordonez.francisco@gmail.com>
2021-01-20 19:56:54 +0100

    fin rama fran

--- juegos.php
+++ juegos.php
@@ -17,12 +17,12 @@
 
             <div class="col-sm-4">
                 <div class="card" style="width:400px">
-                    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                    <img class="card-img-top" src="../media/images/miniaturaFran.png" alt="Card image" style="width:100%">
                     <div class="card-body">
-                        <h4 class="card-title">nombre juego</h4>
-                        <p class="card-text">descripcion
+                        <h4 class="card-title titulosPrincipal font-weight-bold text-center">The Weather Attack</h4>
+                        <p class="card-text" id="descripciones">¡El día se ha complicado con la tormenta! Intenta esquivar a los elementos meteorológicos todo lo que puedas. ¡No va a parar! Que tengas suerte...
                         </p>
-                        <a href="#" class="btn btn-login">Jugar</a>
+                        <a href="../juegos/Francisco/index.html" class="btn btn-login">Jugar</a>
                     </div>
                 </div>
             </div>
