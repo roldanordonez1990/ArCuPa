9eda7dd30b68410d158bf5120ac502c4bf45a712
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-01-31 13:52:36 +0100

    config in progress

--- includes/a_config.php
+++ includes/a_config.php
@@ -1,5 +1,17 @@
 <?php
-	switch ($_SERVER["SCRIPT_NAME"]) {										
+	switch ($_SERVER["SCRIPT_NAME"]) {
+		case "/login.php":
+			$CURRENT_PAGE = "Login"; 
+			$PAGE_TITLE = "Iniciar Sesión";
+			break;
+		case "/contacto.php":
+			$CURRENT_PAGE = "Contacto"; 
+			$PAGE_TITLE = "Contáctanos";
+			break;
+		case "/juegos.php":
+			$CURRENT_PAGE = "Juegos"; 
+			$PAGE_TITLE = "Juegos";
+			break;											
 		default:
 			$CURRENT_PAGE = "Index";
 			$PAGE_TITLE = "ElGranizo - Web de Meteorología";
