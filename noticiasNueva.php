b457a90edd5b78c8f66d590e9badb29d05ef9d4a
Francisco Roldan <roldanordonez.francisco@gmail.com>
2021-02-02 20:23:33 +0100

    ahora si

--- noticiasNueva.php
+++ noticiasNueva.php
@@ -15,7 +15,7 @@
         require_once 'controller/controladorNoticias.php';
        
         if (isset($_POST['publicar'])) {
-
+            $bandera = false;
             $fechaActual = date('Y-m-d');
             if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
                 $noticia = new Noticias();
@@ -24,11 +24,19 @@
                 move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta);
                 $noticia->nuevaNoticia($ruta, $_POST['tituloNoticia'], 1, $fechaActual, $_POST['contenido']);
                 ControladorNoticias::insertNoticia($noticia);
+                ?>
+        <div class="alert alert-success text-center">
+            <strong>Tu noticia ha sido publicada correctamente</strong> 
+        </div>
+        <?php
+
             } else {
                 echo 'ERROR al cargar la imagen';
             }
         }
         ?>
+
+
         <div class="container p-0">
             <div class="row">
                 <div class="col-12 titulosPrincipal font-weight-bold text-center mt-3 mb-3">Nueva Noticia</div>
