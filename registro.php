155a2739493418f95d5afc7a2af533b30d9a89b6
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-02-03 10:11:47 +0100

    register done

--- registro.php
+++ registro.php
@@ -44,71 +44,58 @@
                 </nav>
                     <div class="col login-sec">
                         <h2 class="text-center">Registro</h2>
-                        <form class="login-form" action="" method="post">
-                        <div class="row ">
-                            <div class="col form-group" id="input_login">
-                                <label for="exampleInputEmail1" class="text-uppercase">Usuario</label>
-
-                                <input type="email" name="usuario" class="form-control" placeholder="" required>
-
-
-                            </div>
-                            <div class="col form-group" id="input_login">
-                                <label for="exampleInputPassword1" class="text-uppercase">Contraseña</label>
-
-                                <input type="password" name="password" class="form-control" placeholder=""  minlength="8" maxlength="40" required>
-
-                            </div>
-                        </div>
-                    
-                        <div class="row ">
-                            <div class="col">
-                                <div class="form-group" id="input_login">
-                                    <label for="exampleInputEmail1" class="text-uppercase">Nombre</label>
-
-                                    <input type="text" name="nombre" class="form-control" placeholder="" required>
-
+                        <form action="" method="post" class="needs-validation login-form" novalidate>
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-6  form-group">
+                                <label for="uname" class="text-uppercase">Correo electrónico:</label>
+                                <input type="email" class="form-control" id="uname"  name="usuario" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca un usuario de tipo _@_.com</div>
                                 </div>
-                           
-                            </div>
-                            <div class="col">
-                                <div class="form-group" id="input_login">
-                                    <label for="exampleInputEmail1" class="text-uppercase">Apellido</label>
-                                    <input type="text" name="apellido" class="form-control" placeholder="" required>
-                              
+                                <div class="col-sm-12 col-lg-6 ">
+                                <label class="text-uppercase" for="pwd">Contraseña:</label>
+                                <input type="password" class="form-control" id="pwd"  name="password" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca una contraseña</div>
                                 </div>
                             </div>
-                        </div>
-
-                       
-                        <div class="row ">
-                            <div class="col form-group" id="input_login">
-                                <label for="exampleInputEmail1" class="text-uppercase">Fecha de Nacimiento</label>
-
-                                <input type="date" name="fechaNac" class="form-control" placeholder="" required>
-
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-6   form-group">
+                                <label class="text-uppercase" for="uname">Nombre:</label>
+                                <input type="text" class="form-control" id="uname"  name="nombre" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca un nombre</div>
+                                </div>
+                                <div class="col-sm-12 col-lg-6 ">
+                                <label class="text-uppercase" for="pwd">Apellido:</label>
+                                <input type="text" class="form-control" id="pwd"  name="apellido" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca un apellido</div>
+                                </div>
                             </div>
-                            <div class="col form-group" id="input_login">
-                                <label for="exampleInputEmail1" class="text-uppercase">País</label>
-
-                                <input type="text" name="pais" class="form-control" placeholder="" required>
-
-
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-4   form-group">
+                                <label  class="text-uppercase" for="uname">Fecha de Nacimiento:</label>
+                                <input type="date" class="form-control" id="uname"  name="fechaNac" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca una fecha de nacimiento</div>
+                                </div>
+                                <div class="col-sm-12 col-lg-4 ">
+                                <label class="text-uppercase" for="pwd">País:</label>
+                                <input type="text" class="form-control" id="pwd"  name="pais" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca un país</div>
+                                </div>
+                                <div class="col-sm-12 col-lg-4 ">
+                                <label class="text-uppercase" for="pwd">Teléfono:</label>
+                                <input type="number" class="form-control" id="pwd" name="telefono" required>
+                                <div class="valid-feedback">Correcto</div>
+                                <div class="invalid-feedback">Por favor introduzca un teléfono</div>
+                                </div>
                             </div>
-                            <div class="col form-group" id="input_login">
-                                <label for="exampleInputEmail1" class="text-uppercase">Teléfono</label>
-
-                                <input type="number" name="telefono" class="form-control" placeholder="" required>
-                            </div>
-                        </div>
-
-                        
                             <div class="form-check">
-                                <button name="enviar" type="submit" class="btn btn-login float-right">Enviar</button>
-
+                                <button name="enviar" type="submit" class="btn mt-3 btn-login float-right">Enviar</button>
                             </div>
-
-                        
                         </form>
                         <div class="copy-text">© 2020 Copyright: ArCuPa</div>
                     </div>
@@ -120,6 +107,30 @@
             </div>
         </section>
     </main>
+
+    <div class="container">
+  
+</div>
+
+
+
+    <script>
+        (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+            });
+        }, false);
+        })();
+</script>
 </body>
 
 </html>
