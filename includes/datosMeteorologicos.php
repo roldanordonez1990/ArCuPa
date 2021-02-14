181309ac59aa3fb2aec6353064668187c07f2d63
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-02-14 13:31:15 +0100

    widget in progress

--- includes/datosMeteorologicos.php
+++ includes/datosMeteorologicos.php
@@ -1,40 +1,40 @@
 <?php
 
 //Desplegaremos con $server->service nuestro servicio web
-$cordoba=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Cordoba,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$cordoba=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Cordoba,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoCordoba= json_decode($cordoba);
 //
-$malaga=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Malaga,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$malaga=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Malaga,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoMalaga= json_decode($malaga);
 //
-$sevilla=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Sevilla,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$sevilla=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Sevilla,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoSevilla= json_decode($sevilla);
 //
-$granada=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Granada,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$granada=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Granada,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoGranada= json_decode($granada);
 //
-$almeria=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Almeria,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$almeria=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Almeria,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoAlmeria= json_decode($almeria);
 //
-$huelva=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Huelva,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$huelva=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Huelva,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoHuelva= json_decode($huelva);
 //
-$cadiz=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Cadiz,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$cadiz=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Cadiz,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoCadiz= json_decode($cadiz);
 //
-$jaen=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Jaen,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$jaen=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Jaen,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoJaen= json_decode($jaen);
 //
-$madrid=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Madrid,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$madrid=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Madrid,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoMadrid= json_decode($madrid);
 //
-$barcelona=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$barcelona=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Barcelona,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoBarcelona= json_decode($barcelona);
 //
-$valencia=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Valencia,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$valencia=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Valencia,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoValencia= json_decode($valencia);
 //
-$bilbao=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Bilbao,es&APPID=a19283761f3113b225b7189fb712ca3e&lang=es&units=metric");
+$bilbao=file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=Bilbao,es&APPID=574ee79fa8e2ab5b29c4a108d66187c4&lang=es&units=metric");
 $tiempoBilbao= json_decode($bilbao);
 
 ?>
