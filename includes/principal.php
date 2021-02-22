c34bbc1f91cc13392835a28852b7ead9349c3b3c
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-02-22 13:30:36 +0100

    Update principal.php

--- includes/principal.php
+++ includes/principal.php
@@ -5,7 +5,7 @@ include("includes/datosMeteorologicos.php");?>
 
         <div class="col-sm-8">
             <div class="row mt-4">
-                <div class="titulosPrincipal font-weight-bold col-sm-12 text-left">Mapa actual de la situación
+                <div class="titulosPrincipal font-weight-bold col-sm-12 text-left mt-1">Mapa actual de la situación
                     meteorológica en España</div>
                     <?php include("mapa.php"); ?>
             </div>
